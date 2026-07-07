8f7bf549e3e299e64ef8cb2d8ca8e27b744248d3
GirelliDev <sf2943642@gmail.com>
2026-07-07 14:32:21 -0300

    feat: migrate NPSharp to Electron

--- src/renderer/components/TerminalSource.ts
+++
@@ -1,3 +0,0 @@
-// isso aqui não e arquivo de código. é bloco de notas, pra não abrir o keep agora.
-// Código não será usado em nenhum momento, só tenho um curioso do meu lado. melhor cuidar
-// aparentemente gosta de rosa
---
+++ src/renderer/internal/terminalNotes.ts
@@ -0,0 +1,3 @@
+// isso aqui não e arquivo de código. é bloco de notas, pra não abrir o keep agora.
+// Código não será usado em nenhum momento, só tenho um curioso do meu lado. melhor cuidar
+// aparentemente gosta de rosa
